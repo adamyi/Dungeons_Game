7ad7f640ac31c6018d0c519fabd8bfd432baebb3
Adam Yi <i@adamyi.com>
2019-10-28 21:25:13 +1100

    add example unit test

---
+++ src/test/unsw/dungeon/BasicTest.java
@@ -0,0 +1,13 @@
+package unsw.dungeon;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BasicTest {
+
+  @Test
+  public void oneEqualsToOne() {
+    assertEquals(1, 1);
+  }
+}
